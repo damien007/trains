99c67880a3b730e07d6bbbcc58ffabf1eea0bb11
Matthew Randle <randlematthew@yahoo.com.au>
2017-05-04 11:26:18 +1000

    Added filelayout and descriptions

---
+++ tests.js
@@ -0,0 +1,9 @@
+/**
+ * Functions for testing the basic functionality of the
+ * classes and functions in 'trains.js'
+ */
+(function(globals){
+
+    "use strict";
+
+}(this));
---
+++ trains.js
@@ -0,0 +1,11 @@
+/**
+ * Simulates a map of railway stations in the form of a
+ * connected weighted graph. Provides functions for computing the distance
+ * along a certain route, the number of different routes between two 
+ * stations, and the shortest route between two stations.
+ */
+(function(globals){
+
+    "use strict";
+
+}(this));
